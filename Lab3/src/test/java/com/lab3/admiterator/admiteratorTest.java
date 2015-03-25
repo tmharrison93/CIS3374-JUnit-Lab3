a4f5f923cee2bda97b1e5d63b824e0c68812e567
tue74192 <tue74192@CIS-SE35924.tu.temple.edu>
2015-03-25 10:39:57 -0400

    Lab3 Updted

---
+++ Lab3/src/test/java/com/lab3/admiterator/admiteratorTest.java
@@ -0,0 +1,14 @@
+package com.lab3.admiterator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class admiteratorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Lab3/src/test/java/com/lab3/runnable/runnableTest.java
@@ -0,0 +1,14 @@
+package com.lab3.runnable;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+//@RunWith(JUnitParamsRunner.class)
+public class runnableTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
